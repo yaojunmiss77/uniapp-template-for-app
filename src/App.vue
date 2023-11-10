681dac1323c1ed5c59da9f49dd3818f17be705d9
yaojun <yaoj1@getui.com>
2023-11-10 20:21:37 +0800

    feat: 整理App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <script>
   import { addErrorLogs } from 'uniapp-log-sdk';
   import { requestInstance } from '@/requests';
+
   export default {
     onLaunch: function (options) {
       console.log('小程序启动入参：', options);
-      let opinions = {
-        // #ifdef H5
-        baseUrl: '',
-        // #endif
-        /** 如果在非H5的条件下，适用于真机调试模拟 */
-        // #ifndef H5
-        baseUrl: 'http://dev-oa.ge.cn',
-        // #endif
-        userInfo: {},
-      };
+      let baseUrl = '';
+      /** 如果在非H5的条件下，适用于真机调试模拟，真是调试情况下自行改动 */
+      // #ifndef H5
+      baseUrl = 'http://dev-oa.ge.cn';
+      // #endif
       /** 代表是从客户端启动的 */
       if (options.referrerInfo.extraData.baseUrl) {
+        const { cookie, userInfo, baseUrl: outBaseUrl } = options.referrerInfo.extraData;
+        baseUrl = outBaseUrl;
         uni.setStorageSync('isDebug', false);
-        opinions = options.referrerInfo.extraData;
-        uni.setStorageSync('cookie', opinions.cookie);
-        uni.setStorageSync('userInfo', opinions.userInfo);
+        uni.setStorageSync('cookie', cookie);
+        uni.setStorageSync('userInfo', userInfo);
       } else {
+        /** 代表是开发调试模式 */
         uni.setStorageSync('isDebug', true);
-        opinions.userInfo = uni.getStorageSync('opinions').userInfo || {};
       }
-      uni.setStorageSync('opinions', opinions);
-      uni.setStorageSync('baseUrl', opinions.baseUrl);
+      uni.setStorageSync('extraData', options.referrerInfo.extraData);
+      uni.setStorageSync('baseUrl', baseUrl);
       requestInstance.setParams({
-        baseUrl: `${opinions.baseUrl}/api/`,
+        baseUrl: `${baseUrl}/api/`,
         token: uni.getStorageSync('cookie'),
       });
     },
