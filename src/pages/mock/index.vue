9c7cc4859a8c30f45375cf80fbd2cf0577e8b80b
yaojun <yaoj1@getui.com>
2023-11-11 10:23:38 +0800

    feat: 增强模拟登录功能，支持在cookie方式下，真机模拟登录

--- src/pages/mock/index.vue
+++ src/pages/mock/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <oa-login @login-success="loginSuccess"></oa-login>
+  <oa-login @login-success="loginSuccess" type="cookie"></oa-login>
 </template>
 
 <script setup lang="ts">
   const loginSuccess = (params: any) => {
-    const { userInfo } = params;
+    const { userInfo, session } = params;
+    uni.setStorageSync('cookie', `session=${session};`);
     uni.setStorageSync('userInfo', userInfo);
     uni.redirectTo({
       url: '/pages/index/index',
